3f28364d4483b79b39c94e749e609afa624ecaa2
Mohamad Faid Fadjri <54530737+FaidFadjri@users.noreply.github.com>
2019-08-27 15:35:02 +0700

    Add files via upload

---
+++ Sensor_PIR.ino
@@ -0,0 +1,33 @@
+int pinLED = 8;
+int pinPIR = 7;
+int statusPin = 0;
+#define NOTE_C4 262
+
+const int speaker = 9;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pinLED, OUTPUT);
+  pinMode(pinPIR, INPUT);
+  Serial.begin(9600);
+  pinMode(speaker, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  statusPin = digitalRead(pinPIR);
+  if(statusPin == HIGH){
+    digitalWrite(pinLED, HIGH);
+    Serial.println("Ada Gerakan");
+    tone(speaker, NOTE_C4);
+    delay(250);
+    noTone(speaker);
+    delay(250);
+  }else{
+    digitalWrite(pinLED, LOW);
+    Serial.println("kondisi aman");
+    noTone(speaker);
+  }
+  
+
+}
